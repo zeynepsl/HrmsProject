1d3129cef5860c23511bd89243f0cdbe12b8596f
zeynepsl <zeyneprsalman@gmail.com>
2021-05-29 16:29:23 +0300

    adding files

--- hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/JobPositionDao.java
+++ hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/JobPositionDao.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import kodlamaio.hrms.entities.concretes.JobPosition;
 
-public interface JobPositionDao extends JpaRepository<JobPosition, Integer> {
+public interface JobPositionDao extends JpaRepository<JobPosition, Integer>{
 
 }
