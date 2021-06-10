7c26714ee8e8104722017c801ec319939a48d510
zeynepsl <zeyneprsalman@gmail.com>
2021-06-10 22:28:55 +0300

    🔑 for register

---
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/AuthManager.java
@@ -0,0 +1,129 @@
+package kodlamaio.hrms.business.concretes;
+
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kodlamaio.hrms.business.abstracts.AuthService;
+import kodlamaio.hrms.business.abstracts.CandidateService;
+import kodlamaio.hrms.business.abstracts.EmployerService;
+import kodlamaio.hrms.business.abstracts.UserService;
+import kodlamaio.hrms.business.abstracts.VerificationService;
+import kodlamaio.hrms.core.utilities.results.ErrorResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.core.utilities.results.SuccessResult;
+import kodlamaio.hrms.core.utilities.verifications.VerificationCodeService;
+import kodlamaio.hrms.entities.concretes.Candidate;
+import kodlamaio.hrms.entities.concretes.Employer;
+import kodlamaio.hrms.entities.concretes.User;
+
+@Service
+public class AuthManager implements AuthService{
+
+	EmployerService employerService;
+	CandidateService candidateService;
+	UserService<User> userService;
+	
+	VerificationService verificationService; //dogrulama islemleri yaptigimiz sinif
+	
+	VerificationCodeService verificationCodeService;//code ve link gonderme islemlerini yaptigimiz sinif
+	
+	@Autowired
+	public AuthManager(EmployerService employerService, UserService<User> userService, 
+			VerificationService verifiactionService, CandidateService candidateService, VerificationCodeService verificationCodeService) {
+		super();
+		this.employerService = employerService;
+		this.candidateService = candidateService;
+		this.userService = userService;
+		this.verificationService = verifiactionService;
+		
+		
+		this.verificationCodeService = verificationCodeService;
+	}
+	
+	@Override
+	public Result registerForEmployer(Employer employer) {
+		
+		if(!checkIfEmailExists(employer.getEmail())) {
+			return new ErrorResult("bu is veren zaten mevcut");
+		}
+				
+		if(!checkIfEmployerInformationIsFull(employer)) {
+			return new ErrorResult("lutfen tum bilgilerinizi eksiksiz girin");
+		}
+			
+		if(!checkIfEqualEmailAndDomain(employer.getEmail(), employer.getWebsiteAdress())) {
+			return new ErrorResult("e-posta ile domain alani ayni degil");
+		}
+		employerService.add(employer);
+		
+		/*String code = verificationCodeService.sendCode(employer.getEmail()).getData();//aktivasyon kodunu donduruyor
+		 LocalDate nowDate = LocalDate.now();  
+		verificationService.add(code, true, nowDate);	*/
+			
+		return new SuccessResult("basarili giris - isveren kaydedildi");
+		
+	}
+
+	@Override
+	public Result registerForCandidate(Candidate candidate) {
+		if(!checkIfEmailExists(candidate.getEmail())) {
+			return new ErrorResult("bu aday zaten mevcut");
+		}
+		
+		if(!CheckIfCandidateInformationIsFull(candidate)) {
+			return new ErrorResult("lutfen tum bilgilerinizi eksiksiz girin");
+		}
+		candidateService.add(candidate);
+		String code = verificationCodeService.sendCode(candidate.getEmail()).getData();//aktivasyon kodunu donduruyor 
+		verificationService.add(code, true, LocalDateTime.now());	
+		return new SuccessResult("aday kaydedildi");
+		
+	}
+	
+	
+	//Employer icin is kurallari - BASLANGIC
+	
+	private boolean checkIfEqualEmailAndDomain(String email, String website) {
+		String[] emailToCheck = email.split("@"); //mail adresini @ ifadesine gore bolduk ve emailToCheck dizisine atadik
+		                                          //ornek: zeynep@zeynep.com --> [ "zeynep", "zeynep.com"		
+		if (website.contains(emailToCheck[1])) {
+			return true;
+		}
+		return false;
+	}
+	
+	
+	
+	private boolean checkIfEmployerInformationIsFull(Employer employer) {
+		if(employer.getCompanyName() != null && employer.getPassword() != null && employer.getPhoneNumber() != null && 
+				employer.getWebsiteAdress() != null && employer.getEmail() != null) {
+			return true;
+		}
+		return false;
+	}
+	
+	
+	//ortak is kurallari
+	
+	private boolean checkIfEmailExists(String email) {
+		if(userService.getUserByEmail(email).isSuccess()) {
+			return true;
+		}
+		return false;
+	}
+	
+	//Candidate ici is kurallari - BASLANGİC
+	private boolean CheckIfCandidateInformationIsFull(Candidate candidate) {
+		if(candidate.getEmail() != null && candidate.getFirstName() != null && candidate.getLastName() != null
+				&& candidate.getNationalityId() != null && candidate.getPassword() != null){
+			return true;
+		}
+		return false;
+	}
+
+}
